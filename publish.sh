7f24fb07bf44c911729add87d8cc6af0262853ee
abdfnx <abdfnabd@gmail.com>
2022-07-16 14:56:23 +0300

    bump to v1.7.841 version

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ vim ./discord/__init__.py
 git add .
 git commit -m "bump to $1 version"
 git push
-python3 -m build
+python -m build
 twine check dist/*
 twine upload dist/*
 rm -rf discord_py_api.egg-info
